4bcb008d264bd076492446e39e7f2a2a4a829d61
acil <yunanthaagchrisnaputra@gmail.com>
2023-03-28 07:35:01 +0700

    commit pertama

--- src/App.js
+++ src/App.js
@@ -1,23 +1,10 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="bg-red">
+      <header className="container max-w-3xl mx-auto flex flex-row">Hellow</header>
+      <h1 className="text-5xl">babi</h1>
     </div>
   );
 }
